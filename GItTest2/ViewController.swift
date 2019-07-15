96563663423fa3310764f16c1acff0ecc69d79dc
Saurabh Chandra Bose <saurabh_oldmac@Saurabhs-Mac.local>
2019-07-15 18:03:29 +0530

    Version 1.1
    Add five color

--- GItTest2/ViewController.swift
+++ GItTest2/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomNumber: Int?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        randomNumber = Int.random(in: 1...5)
+        switch randomNumber {
+        case 1:
+            sender.backgroundColor = .blue
+        case 2:
+            sender.backgroundColor = .green
+        case 3:
+            sender.backgroundColor = .red
+        case 4:
+            sender.backgroundColor = .yellow
+        case 5:
+            sender.backgroundColor = .gray
+        default:
+            break
+        }
+    }
 }
 
